317aff82b30b42b8552523c7f5e38bb281cf6b7a
Alex Soimu <fasttrackit.java.online@gmail.com>
2022-10-07 12:27:07 +0300

    07.10.2022

--- src/test/java/org/fasttrackit/RegisterTest.java
+++ src/test/java/org/fasttrackit/RegisterTest.java
@@ -3,7 +3,12 @@ package org.fasttrackit;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class RegisterTest {
 
@@ -24,5 +29,14 @@ public class RegisterTest {
         driver.findElement(By.name("password")).sendKeys("Selenium");
         driver.findElement(By.name("confirmation")).sendKeys("Selenium");
         driver.findElement(By.className("checkbox")).click();
+        driver.findElement(By.xpath("//div[@class='account-create']//button[@title='Register']")).click();
+
+        WebElement successMessageContainer = driver.findElement(By.className("success-msg"));
+
+        assertThat("Confirmation message not displayed.", successMessageContainer, notNullValue());
+        assertThat("Unexpected confirmation message.", successMessageContainer.getText(),
+                is("Thank you for registering with Madison Island."));
+
+        driver.quit();
     }
 }
